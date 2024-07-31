a08e76795986cc1d25efa0a52f570e959b798d5c
swamysurya <swamysurya1@gmail.com>
2024-07-31 10:13:32 +0530

    protected Route is add

---
+++ src/components/ProtectedRoute/index.jsx
@@ -0,0 +1,23 @@
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+  console.log(rest)
+  const navigate = useNavigate();
+  const token = Cookies.get('jwt_token');
+
+  useEffect(() => {
+    if (token === undefined) {
+      navigate('/login');
+    }
+  }, [navigate, token]);
+
+  if (token === undefined) {
+    return null; // or some loading indicator
+  }
+
+  return <Component {...rest} />;
+};
+
+export default ProtectedRoute;
