89fc3e4b4998d9599e76c207d72a4302992ac53a
swamysurya <swamysurya1@gmail.com>
2024-08-02 11:44:57 +0530

    home page added
    section 3 section are add
    editors piks, generes and moods
    , new reselease section

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,21 @@
+import EditorSection from "../EditorSection";
+import GeneresSection from "../GeneresSection";
+import NewRelaseSection from "../NewReleaseSection";
+import SideNavBar from "../SideNavBar";
 
-
+//import css here
+import "./index.css";
 const Home = () => {
+  return (
+    <div className="home-section-container">
+      <SideNavBar />
+      <div>
+        <EditorSection />
+        <GeneresSection />
+        <NewRelaseSection />
+      </div>
+    </div>
+  );
+};
 
-    return <h1>home page</h1>
-}
-
-export default Home
+export default Home;
